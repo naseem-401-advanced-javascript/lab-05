f435f23da4393a51a2b867a1d74b05a2ca1343ac
naseem <izzatnaseem30@gmail.com>
2020-01-25 23:05:34 +0200

    firt submission

---
+++ index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+// Require your model
+
+// Mongoose Server URI
+const MONGOOSE_URI = 'mongodb://localhost:27017/class05';
+
+// Connect
+mongoose.connect(MONGOOSE_URI, { useUnifiedTopology: true, useNewUrlParser: true });
+
+// Do some work
+const Categories = require('./models-singular/categories.js');
+const categories = new Categories();
+
+/**
+ * @param
+ * @returns
+ */
+async function doWork() {
+  await categories.delete(9);
+  await categories.create(
+    {
+      _id: '9',
+      name: 'John',
+      description: 'person',
+    },
+  );
+  await categories.update(9,
+    {
+      name: 'Peach',
+    },
+  );
+
+  const fetched = await categories.get(9);
+  console.log(fetched);
+}
+
+doWork().then(() => {
+  // Disconnect
+  mongoose.disconnect();
+  console.log('disconnect');
+}).catch(err => {
+  console.log('Error: ',err);
+  mongoose.disconnect();
+  console.log('disconnect');
+})
